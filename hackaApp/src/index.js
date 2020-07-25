02e8379c16ab3edac50aa893f91d4e928fe9f289
Marcos Willian <markusuuuu@gmail.com>
2020-07-25 01:38:34 -0300

    mobile initial commit - setup

---
+++ hackaApp/src/index.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import * as Linking from 'expo-linking';
+
+export default function App() {
+  const prefix = Linking.makeUrl('/');
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
