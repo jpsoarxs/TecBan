f6d6cbf85b3aeb130883a002d467a3a0c8730088
João Pedro <49282011+jpsoarxs@users.noreply.github.com>
2020-07-26 10:45:13 -0300

    added Balance router

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,18 +3,23 @@ import express from 'express';
 import ConsentControllers from '../controllers/ConsentControllers';
 import PropertiesControllers from '../controllers/PropertiesControllers';
 import AccountControllers from '../controllers/AccountControllers';
+import BalanceControllers from '../controllers/BalanceControllers';
 
 const routes = express.Router();
 
 const consent = new ConsentControllers();
 const properties = new PropertiesControllers();
 const account = new AccountControllers();
+const balance = new BalanceControllers();
 
 routes.post('/consent', consent.create);
 routes.post('/auth', consent.index)
 
 routes.get('/account', account.index)
 routes.get('/account/:id', account.show)
+
+routes.get('/balance', balance.index)
+routes.get('/balance/:id', balance.show)
 
 routes.get('/properties', properties.index)
 routes.get('/properties/:id', properties.show)
