ca1d7df1b23bc88142d58b2a1520276d34c30812
João Pedro <49282011+jpsoarxs@users.noreply.github.com>
2020-07-26 04:59:31 -0300

    updated routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,13 +1,7 @@
 import express from 'express';
 
 import ConsentControllers from '../controllers/ConsentControllers';
-<<<<<<< HEAD
 import AuthControllers from '../controllers/AuthControllers';
-=======
-
-import userRoutes from './user.routes';
-import SessionRoutes from './session.routes';
->>>>>>> 48f36eb2520c286671b929cedfe0baf83f156b47
 
 const routes = express.Router();
 
@@ -16,11 +10,6 @@ const auth = new AuthControllers();
 
 routes.post('/consent', consent.create);
 
-<<<<<<< HEAD
 routes.post('/auth', auth.index)
-=======
-routes.use('/users', userRoutes);
-routes.use('/session', SessionRoutes);
->>>>>>> 48f36eb2520c286671b929cedfe0baf83f156b47
 
 export default routes;
