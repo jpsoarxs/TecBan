0b49a44e4e4603b5fa33f8a2632326c523acd6d3
João Pedro <49282011+jpsoarxs@users.noreply.github.com>
2020-07-26 10:38:34 -0300

    added Account route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,14 +2,19 @@ import express from 'express';
 
 import ConsentControllers from '../controllers/ConsentControllers';
 import PropertiesControllers from '../controllers/PropertiesControllers';
+import AccountControllers from '../controllers/AccountControllers';
 
 const routes = express.Router();
 
 const consent = new ConsentControllers();
 const properties = new PropertiesControllers();
+const account = new AccountControllers();
 
 routes.post('/consent', consent.create);
 routes.post('/auth', consent.index)
+
+routes.get('/account', account.index)
+routes.get('/account/:id', account.show)
 
 routes.get('/properties', properties.index)
 routes.get('/properties/:id', properties.show)
